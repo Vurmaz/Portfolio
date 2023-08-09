192b3f075ddeca3d19c74a5bc138948bdd94d0bd
Vurmaz <sadikvurmaz@gmail.com>
2023-08-09 17:19:27 +0300

    adding resume

--- app/components/contact/contactLinks.js
+++ app/components/contact/contactLinks.js
@@ -14,10 +14,10 @@ export default function ContactLinks() {
         </motion.div>
         <div className='flex gap-16 justify-center p-4 items-center flex-wrap h-auto'>
             <ContactBox icon={<TfiEmail className='w-8 h-8' />} name={'Email'} url={'mailto:info@sadikvurmaz.app'} />
-            <ContactBox icon={<BsTwitter className='w-8 h-8' />} name={'Twitter'} url={''} />
+            <ContactBox icon={<BsTwitter className='w-8 h-8' />} name={'Twitter'} url={'https://twitter.com/sadikXvurmaz'} />
             <ContactBox icon={<BsGithub className='w-8 h-8' />} name={'Github'} url={'https://github.com/Vurmaz'} />
             <ContactBox icon={<BsLinkedin className='w-8 h-8' />} name={'LinkedIn'} url={'https://www.linkedin.com/in/sad%C4%B1k-vurmaz-a57131287/'} />
-            <ContactBox icon={<MdOutlineContactPage className='w-8 h-8' />} name={'Resume'} url={''} />                          
+            <ContactBox icon={<MdOutlineContactPage className='w-8 h-8' />} name={'Resume'} url={'https://flowcv.com/resume/4csw0ks623'} />                          
         </div>
     </div>
   )
