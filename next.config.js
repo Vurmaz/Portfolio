90536dac0b478414a67f1b9b01926afd4e620708
Vurmaz <sadikvurmaz@gmail.com>
2023-08-14 13:38:31 +0300

    s

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.js$/,
-      use: {
-        loader: '@swc/loader',
-        options: {
-          jsc: {
-            parser: {
-              syntax: 'ecmascript',
-              jsx: true
-            },
-            transform: {
-              react: true
-            }
-          }
-        }
-      }
-    });
-    return config;
-  }    
+ 
 }
 
 module.exports = nextConfig
