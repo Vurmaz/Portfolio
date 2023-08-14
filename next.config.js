c41dc50874e34ae9d388310142cdda90f281e021
Vurmaz <sadikvurmaz@gmail.com>
2023-08-14 12:30:15 +0300

    performance issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: {
+        loader: '@swc/loader',
+        options: {
+          jsc: {
+            parser: {
+              syntax: 'ecmascript',
+              jsx: true
+            },
+            transform: {
+              react: true
+            }
+          }
+        }
+      }
+    });
+    return config;
+  }    
+}
 
 module.exports = nextConfig
