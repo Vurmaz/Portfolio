c6d1cab988b9771c0406ddad1b33f34d3195c646
Vurmaz <sadikvurmaz@gmail.com>
2023-08-16 23:25:39 +0300

    changed hero text

--- app/components/hero/heroText.js
+++ app/components/hero/heroText.js
@@ -4,8 +4,9 @@ export default function HeroText() {
   return (
     <div className='flex items-center justify-center md:justify-start'>
         <p className='text-xs xl:text-sm w-7/12 text-center sm:text-left'>
-          I'm a Full Stack Web Developer from Turkiye.
-          I like to build creative, solid and complex applications using the right technologies that benefit both the front and back-end.        
+          I am Sadık a proficient full-stack web developer based in Turkey. 
+          With expertise in both front-end and back-end technologies, 
+          I am capable of creating comprehensive and dynamic web applications.        
         </p>                         
     </div>
   )
