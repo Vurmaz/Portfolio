1250c62f44d08494d7d7a67c21d52369745f0275
Vurmaz <sadikvurmaz@gmail.com>
2023-08-20 23:53:15 +0300

    updated about text

--- app/components/about/aboutText.js
+++ app/components/about/aboutText.js
@@ -17,9 +17,9 @@ export default function AboutText() {
             sm:[--opacity-from:0%] sm:[--opacity-to:100%]
           '                              
       >
-        I am Sadık Vurmaz a determined and skilled Full-Stack Web Developer with 
-        a strong foundation in front-end technologies like HTML, CSS, and JavaScript, 
-        combined with proficiency in back-end development and server-side languages like Node JS and Express.
+        I am Sadık a determined and skilled Full-Stack Web Developer with 
+        the strong foundation in front-end technologies like HTML, CSS, and JavaScript, 
+        combined with proficiency in back-end development and server-side technologies like Node.js and Express.
         My portfolio features a wide range of creative and functional web applications.
         Every project I worked on was committed to producing high-quality code and user-friendly interfaces.<br /><br />
         I began my education during the COVID pandemic and soon discovered that the internet is where I am most passionate. Then I made a hard decision and shifted from environmental engineering to web development.
